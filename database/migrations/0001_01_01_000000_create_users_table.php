0bfb63a10378d903f697ef240ce1ffcfd9a53d38
Mihail Gusev <122309657+Sendrak2003@users.noreply.github.com>
2024-11-14 18:27:37 +0300

    Update 0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('avatar')->nullable();
-            $table->enum('status', ['Публичный', 'Приватный'])->default('приватный');
+            $table->enum('status', ['Публичный', 'Приватный'])->default('Приватный');
             $table->rememberToken();
             $table->timestamps();
         });
